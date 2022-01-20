35b3a8f7851206f37e936ab25c3775817aa9617d
InnaMih212 <miheikinainna212@gmail.com>
2022-01-20 10:01:18 +0300

    1

--- Tasks.js
+++ Tasks.js
@@ -1,13 +1,3 @@
-function positiveSum(arr) {
-    let result = 0;
-
-    arr.forEach( (num) => {
-        if (num > 0) {
-            result += num;
-        }
-    })
-
-    console.log(212);
-    return result;
-
-}
+// inna
+/* inna my
+name  */
