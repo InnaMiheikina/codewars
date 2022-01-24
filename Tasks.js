3047e21d7c2a3c2ea1259c279556b1579d889c40
InnaMih212 <miheikinainna212@gmail.com>
2022-01-24 19:31:35 +0300

    3a

--- Tasks.js
+++ Tasks.js
@@ -1,10 +1,9 @@
-function longest(s1, s2) {
-    let a = [...new Set(s1.split(""))];
-    let b = [...new Set(s2.split(""))];
-    for (let i=0; i<b.length; i++) {
-        a.push(b[i]);
+function isTriangle(a,b,c){
+    if((Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) ||
+        (Math.pow(b, 2) + Math.pow(c, 2) === Math.pow(a, 2)) ||
+        (Math.pow(c, 2) + Math.pow(a, 2) ===Math.pow(b, 2))) {
+        return true;
+    } else {
+        return false;
     }
-    let init_combo = [...new Set(a)];
-    let combo = init_combo.sort().join("");
-    return combo;
 }
