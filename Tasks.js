3b255f3cc12088c1128930d806a80310f3ded6a7
InnaMih212 <miheikinainna212@gmail.com>
2022-01-24 18:07:15 +0300

    3

--- Tasks.js
+++ Tasks.js
@@ -1,15 +1,3 @@
-function DNAStrand(dna){
-    let output = ' ';
-    for (let i=0; i<dna.length; i++){
-        if (dna[i] == 'A') {
-            output += 'T';
-        } else if (dna[i] == 'T'){
-            output += 'A';
-        }else if (dna[i] == 'C'){
-            output += 'G';
-        }else if (dna[i] == 'G'){
-            output += 'C';
-        }
-    }
-    return output;
+function iceBrickVolume(radius, bottleLength, rimLength) {
+    return 2 * Math.pow(radius, 2) * (bottleLength - rimLength);
 }
