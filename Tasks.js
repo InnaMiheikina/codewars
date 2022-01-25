95db42ffb15c9d36059afe35f631e0d542cf37bf
InnaMih212 <miheikinainna212@gmail.com>
2022-01-25 15:50:27 +0300

    14

--- Tasks.js
+++ Tasks.js
@@ -1,9 +1,17 @@
 
-function bouncingBall(h,  bounce,  window) {
-    const cond = h > 0 && (bounce > 0 && bounce < 1) && window < h;
-    if (cond) {
-        return h < window ? -1 : 2 + bouncingBall((h * bounce), bounce, window);
-    } else {
-        return -1;
+function deleteNth(arr,n){
+    var count = 0;
+    for(let i= arr.length; i > 0; i--){
+        for(let j=0; j < arr.length; j++){
+            if (arr[i] == arr[j]){
+                count += 1
+            }
+        }
+        if(count > n){
+            arr.splice(i,1);
+            i = arr.length;
+        }
+        count = 0;
     }
+    return arr;
 }
