4f42884708e6d5150d228da96f480de87bc7c81f
InnaMih212 <miheikinainna212@gmail.com>
2022-01-25 10:19:35 +0300

    9

--- Tasks.js
+++ Tasks.js
@@ -1,25 +1,12 @@
-var uniqueInOrder=function(iterable){
-    if(iterable.length == 0) return[];
-    var stage= [iterable[0]];
-    if(typeof iterable === 'string'){
-        iterable = iterable.split('');
-    }
-    iterable.reduce(function(pre,cur){
-        if(pre != cur) {
-            stage.push(cur)
-        }
-        return cur;
-    });
-    return stage
-}
+function getCount(str) {
+    let vowelsCount = 0;
 
-function uniqueInOrder (it) {
-    var result = []
-    var last
-    for (var i = 0; i < it.length; i++) {
-        if (it[i] !== last) {
-            result.push(last = it[i])
+    const vowels = ["a", "e", "i", "o", "u"]
+
+    for(let char of str) {
+        if (vowels.includes(char)) {
+            vowelsCount++;
         }
     }
-    return result
+    return vowelsCount;
 }
