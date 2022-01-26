bf5657fde42193244162e5840ac69a23d669962f
InnaMih212 <miheikinainna212@gmail.com>
2022-01-26 14:07:43 +0300

    20

--- Tasks.js
+++ Tasks.js
@@ -1,16 +1,8 @@
-function towerBuilder(nFloors) {
-    var finalArray = [];
-    var numSpaces = 0;
-    // Start from the reverse.
-    for (i = nFloors; i > 0; i--) {
-        // At the end, the number of spaces are 0.
-        // Have one and less for the half of the stars.
-        // Add another set of spaces.
-        finalArray.push(" ".repeat(numSpaces) + "*".repeat(i) + "*".repeat(i-1)  + " ".repeat(numSpaces));
-        // Increment the space, while going to top.
-        numSpaces++;
-    }
-    // Reverse the array.
-    return finalArray.reverse();
+function permutations(string) {
+    return (string.length <= 1) ? [string] :
+        Array.from(new Set(
+            string.split('')
+                .map((char, i) => permutations(string.substr(0, i) + string.substr(i + 1)).map(p => char + p))
+                .reduce((r, x) => r.concat(x), [])
+        ));
 }
-console.log(towerBuilder(3));
