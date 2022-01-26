b851eec4e10a57258d6c5b95d62babce966bb026
InnaMih212 <miheikinainna212@gmail.com>
2022-01-26 13:38:36 +0300

    18

--- Tasks.js
+++ Tasks.js
@@ -1,6 +1,12 @@
-function pillars(num_pill, dist, width) {
-    if(num_pill < 2){
-        return 0;
+function isValidWalk(walk) {
+    var Es = walk.filter(a => a == 'e')
+    var Ws = walk.filter(b => b == 'w')
+    var Ns = walk.filter(c => c == 'n')
+    var Ss = walk.filter(d => d == 's')
+
+    if (Es.length == Ws.length && Ns.length == Ss.length && walk.length == 10) {
+        return true
+    } else {
+        return false
     }
-    return num_pill * ((dist * 100) + width) - (width * 2) - (dist * 100);
 }
