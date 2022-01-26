795056a96036babdbbc27e9804dd0a0168d95935
InnaMih212 <miheikinainna212@gmail.com>
2022-01-26 14:11:55 +0300

    21

--- Tasks.js
+++ Tasks.js
@@ -1,8 +1,22 @@
-function permutations(string) {
-    return (string.length <= 1) ? [string] :
-        Array.from(new Set(
-            string.split('')
-                .map((char, i) => permutations(string.substr(0, i) + string.substr(i + 1)).map(p => char + p))
-                .reduce((r, x) => r.concat(x), [])
-        ));
+function score(dice) {
+    // Fill me in!
+    let arr = [0, 0, 0, 0, 0, 0]
+    dice.forEach((item) => {
+        arr[item - 1] = arr[item - 1] + 1
+    })
+    const all = arr.reduce((total, item, index) => {
+        if (index === 0) {
+            const i = item%3
+            const j = (item - item%3)/3
+            return total + 100*i + 1000*j
+        }
+        if (index === 4 && item < 3) {
+            return total + item * 50
+        }
+        if (item < 3) {
+            return total
+        }
+        return total + (index+1) * 100
+    }, 0)
+    return all
 }
