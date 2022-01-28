a5b00d9cb18bb8c7373944c5c11aa4118e6c2f0f
InnaMih212 <miheikinainna212@gmail.com>
2022-01-28 22:19:41 +0300

    32

--- Tasks.js
+++ Tasks.js
@@ -1,29 +1,44 @@
-function nextSmaller(n) {
+var decodeBits = function(bits){
 
-    const arr = ( n + '' ).split( '' ).map( Number );
-    let i, prev = 9;
-    for ( i = arr.length; i--; ) {
-        if ( arr[ i ] > prev )
-            break;
-        prev = arr[ i ];
-    }
+    // remove trailing 0s from the beginning and the end
 
-    if ( i < 0 )
-        return -1;
+    bits = bits.replace(/^0+/,'').replace(/0+$/,'')
 
-    const pivot_i = i;
-    const pivot = arr[ pivot_i ];
+    // find the rate by getting the "minimum" cluster of 1s or 0s, corresponding to a 'dot'
+    let rate1s =  bits.split('0').map(c => c.length).filter(c=> c!=0).sort((a,b)=>{return a - b})[0]
+    let rate0s =  bits.split('1').map(c => c.length).filter(c=> c!=0).sort((a,b)=>{return a - b})[0] ||  rate1s
+    let rate = Math.min(rate1s , rate0s);
+    // replace pauses between words
+    let reg = new RegExp("0".repeat(rate*7),"g");
+    let data = bits.replace(reg," ");
 
-    for ( i = arr.length; i--; ) {
-        if ( arr[ i ] < pivot )
-            break;
-    }
+    // replace pauses between characters
+    reg = new RegExp("0".repeat(rate*3),"g");
+    data = data.replace(reg,'|')
+    let words = data.split(" ")
+    let output = []
+    words.forEach(word => {
+        let wordOutput = []
+        word.split('|').forEach(letter => {
+            // get dashes
+            let reg = new RegExp("1".repeat(rate*3),"g");
+            letter = letter.replace(reg,'-');
+            // get dots
+            reg = new RegExp("1".repeat(rate*1),"g");
+            letter = letter.replace(reg,'.')
 
-    arr[ pivot_i ] = arr[ i ];
-    arr[ i ] = pivot;
+            // remove 0s
+            letter = letter.replace(/0/g,'')
+            wordOutput.push(letter)
+        });
+        output.push(wordOutput);
 
-    if ( arr[ 0 ] === 0 )
-        return -1;
+    })
+    return output;
+}
 
-    return +arr.slice( 0, pivot_i + 1 ).concat( arr.slice( pivot_i + 1 ).reverse( ) ).join('');
+var decodeMorse = function(morseCode){
+    return morseCode.map(word => {
+        return word.map(letter => MORSE_CODE[letter]).join('')
+    }).join(' ');
 }
