c89c9a78951b63428a291822387064d9c1e6d1fb
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-02-01 12:11:16 +0300

    3

--- Tasks.js
+++ Tasks.js
@@ -1,12 +1,3 @@
-function positiveSum(arr) {
-    let result = 0;
-
-    arr.forEach( (num) => {
-        if (num > 0) {
-            result += num;
-        }
-    })
-
-    return result
-
+function multiply(a, b){
+    return a * b
 }
