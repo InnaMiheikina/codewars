06ff5caa6eb2a219517c83e95b20429f00c0d2c5
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-02-02 10:37:01 +0300

    13av

--- Tasks.js
+++ Tasks.js
@@ -1,18 +1,8 @@
-function validBraces(braces){
-    let tracer = []
-    for(let i=0;i < braces.length; i++){
-        if ( braces[i] === "(" || braces[i] === "{" || braces[i] === "["){
-            tracer.push(braces[i])
-        } else{
-            if(tracer.length === 0) return false
-            let lastValue = tracer[tracer.length-1]
-            if( (braces[i] === ']' && lastValue === '[') || (braces[i] === '}' && lastValue === '{') || (braces[i] === ')' && lastValue === '('))
-            {
-                tracer.pop()
-            } else {
-                break;
-            }
-        }
+function bouncingBall(h,  bounce,  window) {
+    const cond = h > 0 && (bounce > 0 && bounce < 1) && window < h;
+    if (cond) {
+        return h < window ? -1 : 2 + bouncingBall((h * bounce), bounce, window);
+    } else {
+        return -1;
     }
-    return tracer.length === 0
 }
