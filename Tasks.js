37de3dce97abb49c0c89b6120c1fcb73932d7457
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-02-02 10:39:33 +0300

    15av

--- Tasks.js
+++ Tasks.js
@@ -1,8 +1,15 @@
-function digPow(n, p) {
+
+function digPow(n, p){
     var digits = n.toString().split('');
     var result = 0;
-    for (var i = 0; i < digits.length; i++) {
+    for(var i=0; i<digits.length; i++){
         result = result + Math.pow(digits[i], p);
         p++;
+    }
+    var data = result/n;
+    if(result % n === 0){
+        return data;
+    }else{
+        return -1;
     }
 }
