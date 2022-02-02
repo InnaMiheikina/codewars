2d0009319567e0298c9bf8bb76a837a8e8cbc268
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-02-02 10:33:27 +0300

    8av

--- Tasks.js
+++ Tasks.js
@@ -1,10 +1,25 @@
-function findOdd(A) {
-    for (let i = 0; i <= A.length-1; i += 1) {
+var uniqueInOrder=function(iterable){
+    if(iterable.length == 0) return[];
+    var stage= [iterable[0]];
+    if(typeof iterable === 'string'){
+        iterable = iterable.split('');
+    }
+    iterable.reduce(function(pre,cur){
+        if(pre != cur) {
+            stage.push(cur)
+        }
+        return cur;
+    });
+    return stage
+}
 
-        let elmLength = A.filter ((elm) => elm === A[i]).length;
-
-        if (elmLength % 2 !== 0) {
-            return A[i];
+function uniqueInOrder (it) {
+    var result = []
+    var last
+    for (var i = 0; i < it.length; i++) {
+        if (it[i] !== last) {
+            result.push(last = it[i])
         }
     }
+    return result
 }
