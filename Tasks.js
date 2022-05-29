0e4d9aeaed69bbdf7798cf1bbbdee99819823cab
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-05-29 19:12:41 +0300

    i % 3 === 0 || i % 5 === 0

--- Tasks.js
+++ Tasks.js
@@ -1,16 +1,16 @@
-function towerBuilder(nFloors) {
-    var finalArray = [];
-    var numSpaces = 0;
-    // Start from the reverse.
-    for (i = nFloors; i > 0; i--) {
-        // At the end, the number of spaces are 0.
-        // Have one and less for the half of the stars.
-        // Add another set of spaces.
-        finalArray.push(" ".repeat(numSpaces) + "*".repeat(i) + "*".repeat(i-1)  + " ".repeat(numSpaces));
-        // Increment the space, while going to top.
-        numSpaces++;
+const calcDevidedBy3And5 = (number) => {
+
+    if (number < 0) return 0;
+
+    let sum = 0;
+
+    for (let i = 0; i < number; ++i) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
     }
-    // Reverse the array.
-    return finalArray.reverse();
+
+    return sum;
 }
-console.log(towerBuilder(3));
+
+console.log(calcDevidedBy3And5(6))
