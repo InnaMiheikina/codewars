61e7d6bbe78dbffaa09c8419da0157ca67d4cadf
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-07-15 18:12:35 +0300

    summation(8) -> 36 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8

--- Tasks.js
+++ Tasks.js
@@ -1,5 +1,10 @@
-//Simple, remove the spaces from the string, then return the resultant string.
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 
-function noSpace(x){
-    return x.split(' ').join('');
+var summation = function (num) {
+    let foo = []
+    for (let i = 0; i <= num; i++) {
+       foo.push(i)
+    }
+    return foo.reduce((acc,el)=>acc+el, 0)
 }
+console.log(summation(8))
