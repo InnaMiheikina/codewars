ea688e41293d2f670830de73cf9f339f58336dce
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-07-18 07:40:55 +0300

    ('+', 4, 7) --> 11 ('-', 15, 18) --> -3

--- Tasks.js
+++ Tasks.js
@@ -1,9 +1,22 @@
+//Your task is to create a function that does four basic mathematical operations.
+//
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+function basicOp(operation, value1, value2) {
+    let sum= 0;
+   if(operation ==='+') {
+      let sum = value1 + value2
+       return sum;
+   }else if(operation=== '-' ){
+       let sum = value1-value2;
+       return sum;
+   } else if(operation=== '*'){
+       let sum = value1*value2;
+       return sum;
+   }else if(operation=== '/' ){
+       let sum = value1/value2;
+       return sum;
+   }
+}
 
-//Given an array of integers your solution should find the smallest integer.
-
-   function findSmallestInt(args) {
-      args.sort((a,b) => a-b)
-       return args[0]
-            }
-
-   console.log(findSmallestInt([12,13,11]))
+console.log(basicOp('/', 10,2))
